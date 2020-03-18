de5ea14539bef81371025f203c2de5a97e51a7e4
Vladyslav94 <grigoren_vlad2009@yahoo.com>
2020-03-18 17:38:31 +0200

    creating window with HTML and Text pannels

--- View.java
+++ View.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
 import java.awt.*;
@@ -42,6 +43,15 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void initEditor() {
+        htmlTextPane.setContentType("text/html");
+        JScrollPane jScrollPane = new JScrollPane(htmlTextPane);
+        tabbedPane.add("HTML", jScrollPane);
+        JScrollPane jScrollPane1 = new JScrollPane(plainTextPane);
+        tabbedPane.add("Текст", jScrollPane1);
+        tabbedPane.setPreferredSize(new Dimension(600, 300));
+        tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
+
     }
 
     public void initGui() {
@@ -49,6 +59,8 @@ public class View extends JFrame implements ActionListener {
         initEditor();
         pack();
     }
+
+    public void selectedTabChanged(){}
 
 
 }
