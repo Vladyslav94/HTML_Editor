46be80aa5487fdb71693f799cde15115e55906e1
Vladyslav94 <grigoren_vlad2009@yahoo.com>
2020-03-19 12:15:14 +0200

    can undo & redo buttons added

--- listeners/UndoMenuListener.java
+++ listeners/UndoMenuListener.java
@@ -7,13 +7,26 @@ import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 
 public class UndoMenuListener implements MenuListener {
+    private View view;
+    private JMenuItem undoMenuItem;
+    private JMenuItem redoMenuItem;
 
     public UndoMenuListener(View view, JMenuItem undoMenuItem, JMenuItem redoMenuItem){
-
+        this.view = view;
+        this.undoMenuItem = undoMenuItem;
+        this.redoMenuItem = redoMenuItem;
     }
     @Override
-    public void menuSelected(MenuEvent e) {
+    public void menuSelected(MenuEvent menuEvent) {
+        if(view.canUndo() == false){
+            undoMenuItem.setEnabled(false);
+        } else
+            undoMenuItem.setEnabled(true);
 
+        if(view.canRedo() == false){
+            redoMenuItem.setEnabled(false);
+        } else
+            redoMenuItem.setEnabled(true);
     }
 
     @Override
